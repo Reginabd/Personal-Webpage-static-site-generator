cb929ec56de22c0a3cb821fcf79477b27cb7aa11
Regina <reginaa@mail.com>
2021-02-25 21:09:15 -0500

    adds build.sh file

---
+++ build.sh
@@ -0,0 +1,9 @@
+cat ../templates/top.html ../content/index.html ../templates/bottom.html > ../docs/index.html
+
+cat ../templates/top.html ../content/about.html ../templates/bottom.html > ../docs/about.html
+
+cat ../templates/top.html ../content/education.html ../templates/bottom.html > ../docs/education.html
+
+cat ../templates/top.html ../content/experience.html ../templates/bottom.html > ../docs/experience.html
+
+cat ../templates/top.html ../content/skills.html ../templates/bottom.html > ../docs/skills.html
